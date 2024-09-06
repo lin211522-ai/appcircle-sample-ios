de8e225d4ef0cccb1b1815311ac374e24a615944
boztopuz <boztopuz@gmail.com>
2024-09-06 10:36:27 +0300

    Added tuist tool

---
+++ Project.swift
@@ -0,0 +1,21 @@
+//
+//  Project.swift
+//  Appcircle
+//
+//  Created by Burak Öztopuz on 6.09.2024.
+//
+
+import ProjectDescription
+
+
+import ProjectDescription
+
+import ProjectDescription
+
+let project = Project(
+    name: "Appcircle",
+    targets: [
+        /** Targets will go here **/
+    ]
+)
+
---
+++ Tuist/Config.swift
@@ -0,0 +1,11 @@
+//
+//  Config.swift
+//  Appcircle
+//
+//  Created by Burak Öztopuz on 6.09.2024.
+//
+
+import Foundation
+import ProjectDescription
+
+let config = Config()
